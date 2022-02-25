66ad1d864b669e75602c73c84480e96e074edf20
KeerthiMalini05 <58335834+KeerthiMalini05@users.noreply.github.com>
2022-02-25 15:14:06 +0530

    Update settings.py

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -116,7 +116,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
